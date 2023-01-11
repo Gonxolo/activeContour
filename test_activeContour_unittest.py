7eb8268ae8807d08758f125dbd45d7669c0b2cfc
Gonzalo Alarcon <gonzaloalarcon@ug.uchile.cl>
2023-01-11 16:50:33 -0300

    test: added setup values and initialization test

--- test_activeContour_unittest.py
+++ test_activeContour_unittest.py
@@ -1,9 +1,50 @@
 import unittest
+import numpy as np
 from ActiveContour import ActiveContour
 
 class TestActiveContour(unittest.TestCase):
     def setUp(self) -> None:
-        self.activeContour = ActiveContour([[1,2,3],[4,5,6]], [1,2,3], [4,5,6])
+        self.image = [[1,2,3],
+                 [4,5,6]]
+        self.x = [1,2,3]
+        self.y = [4,5,6]
+        self.alpha_min_value = 0.001
+        self.beta_min_value = 0.001
+        self.gamma_min_value = 0.1
+        self.kappa_min_value = 0.0
+        self.mu_min_value = 0.001
+        self.mu_max_value = 0.25
+        self.activeContour = ActiveContour(self.image, self.x, self.y)
+
+    def test_init(self):
+        """
+        Checks that ActiveContour is properly initialized when only image, x
+        and y parameters are given
+        """
+        
+        # activeContour's image, x and y must be the same as the arrays given 
+        # on setUp
+        self.assertTrue(np.array_equal(self.activeContour.image, self.image))
+        self.assertTrue(np.array_equal(self.activeContour.x, self.x))
+        self.assertTrue(np.array_equal(self.activeContour.y, self.y))
+
+        # alpha, beta, gamma, kappa, mu, gvf_iterations and iterations must be 
+        # in their respective ranges when initialized (smaller than the maximum
+        # larger than the minimum)
+        self.assertTrue(self.activeContour.alpha >= self.alpha_min_value)
+        self.assertTrue(self.activeContour.beta >= self.beta_min_value)
+        self.assertTrue(self.activeContour.gamma >= self.gamma_min_value)
+        self.assertTrue(self.activeContour.kappa >= self.kappa_min_value)
+        self.assertTrue(self.mu_max_value >= self.activeContour.mu >= self.mu_min_value)
+        self.assertTrue(self.activeContour.gvf_iterations >= 1)
+        self.assertTrue(self.activeContour.iterations >= 1)
+
+        # [u, v] are initialized as None and the value is assigned in edgeMap()
+        self.assertIsNone(self.activeContour.u)
+        self.assertIsNone(self.activeContour.v)
+
+        # npts must be initialized with a positive integer
+        self.assertTrue(self.activeContour.npts >= 0)
 
     #parab hacer varios asserts en una fun test se usa self.subTest():self.assertBKJa
     def test_laplacian():
