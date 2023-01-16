7b0f7070e84289db6158d3c1934269bcaed1ebdb
Gonzalo Alarcon <gonzaloalarcon@ug.uchile.cl>
2023-01-16 17:30:15 -0300

    test: polygon_perimeter

--- test_geometryFunctions.py
+++ test_geometryFunctions.py
@@ -7,7 +7,47 @@ class TestActiveContour(unittest.TestCase):
         self.vec = np.array([-0.836854, -0.172280, 0.187117, 1.61544, -0.176774])
     
     def test_polygon_perimeter(self):
-        pass
+        
+        x = []
+        y = [1.0, 2.0, 3.0, 4.0, 5.0]
+        self.assertEqual(polygon_perimeter(x, y), -1.0)
+        
+        x = [1.0]
+        y = [1.0, 2.0, 3.0, 4.0, 5.0]
+        self.assertEqual(polygon_perimeter(x, y), 0.0)
+
+        x = [1.0, 2.0, 3.0, 4.0, 5.0]
+        y = []
+        self.assertEqual(polygon_perimeter(x, y), -1.0)
+        
+        x = [1.0, 2.0, 3.0, 4.0, 5.0]
+        y = [1.0]
+        self.assertEqual(polygon_perimeter(x, y), 0.0)
+
+        x = [1.0, 2.0, 3.0, 4.0, 5.0]
+        y = [1.0, 2.0, 3.0, 4.0, 5.0]
+        xy_factor = []
+
+        expected_res = 11.3137
+
+        self.assertAlmostEqual(polygon_perimeter(x, y, xyFactor=xy_factor), expected_res, 4)
+        
+        x = [1.0, 2.0, 3.0, 4.0, 5.0]
+        y = [1.0, 2.0, 3.0, 4.0, 5.0]
+        xy_factor = [0.5]
+
+        expected_res = 5.65685
+
+        self.assertAlmostEqual(polygon_perimeter(x, y, xyFactor=xy_factor), expected_res, 5)
+        
+        x = [1.0, 2.0, 3.0, 4.0, 5.0]
+        y = [1.0, 2.0, 3.0, 4.0, 5.0]
+        xy_factor = [0.1, 0.1]
+
+        expected_res = 1.13137
+
+        self.assertAlmostEqual(polygon_perimeter(x, y, xyFactor=xy_factor), expected_res, 5)
+
 
     def test_calcNorm_L1ForVector(self):
         #Using the same value of self.vec, it returns 1.84551
